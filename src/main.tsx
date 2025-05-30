78aa4ac90538690d830a5030034c6657fb733a12
Hela2612 <heladewaal@gmail.com>
2025-05-30 16:35:06 +0200

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MSLSearchApp from "./MSLSearchApp";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <MSLSearchApp />
+  </React.StrictMode>
+);
